0e235b125c99c60846c105861033f5204e6f9ee7
GhilesLarbi <ghiles.larbi@fgei.ummto.dz>
2023-05-11 01:46:57 +0100

    buy more then 1 ticket

--- scripts/reservation.js
+++ scripts/reservation.js
@@ -6,6 +6,7 @@ let currentBleacher
 
 const ticket = {
     bleacherType: "VIP",
+    gameId: null,
     quantity: 1,
     successUrl: window.location.href.replace("reservation.html", "tickets.html"),
     cancelUrl: window.location.href,
@@ -37,34 +38,33 @@ async function init() {
 
     const buyTicketBtnElm = document.querySelector(".buy-ticket-btn")
 
-    const gameRes = await APP.fetch(`game/${gameIdParam}`, {
+    APP.fetch(`game/${gameIdParam}`, {
         query: {
             include: "team,league"
         }
-    })
-
-    if (!gameRes.success) {
-        document.querySelector(".game-wrapper").classList.add("game-wrapper_err")
-    } else {
-        const game = gameRes.data
-        if (new Date(game.date).getTime() < new Date()) {
+    }).then(gameRes => {
+        if (!gameRes.success) {
             document.querySelector(".game-wrapper").classList.add("game-wrapper_err")
-        }
-        else {
-            ticket.gameId = game.id
-            document.querySelector(".game-date").textContent = APP.formateDate(new Date(game.date))
-            document.querySelector(".game-center p:first-child").textContent = game.league.name
-            document.querySelector(".game-center p:last-child").textContent = game.description
-            document.querySelector(".game-content .team:first-child img").src = `${APP.HOSTNAME}${game.team1.logo}`
-            document.querySelector(".game-content .team:first-child p").textContent = game.team1.name
-            document.querySelector(".game-content .team:last-child img").src = `${APP.HOSTNAME}${game.team2.logo}`
-            document.querySelector(".game-content .team:last-child p").textContent = game.team2.name
+        } else {
+            const game = gameRes.data
+            if (new Date(game.date).getTime() < new Date()) {
+                document.querySelector(".game-wrapper").classList.add("game-wrapper_err")
+            }
+            else {
+                ticket.gameId = game.id
+                document.querySelector(".game-date").textContent = APP.formateDate(new Date(game.date))
+                document.querySelector(".game-center p:first-child").textContent = game.league.name
+                document.querySelector(".game-center p:last-child").textContent = game.description
+                document.querySelector(".game-content .team:first-child img").src = `${APP.HOSTNAME}${game.team1.logo}`
+                document.querySelector(".game-content .team:first-child p").textContent = game.team1.name
+                document.querySelector(".game-content .team:last-child img").src = `${APP.HOSTNAME}${game.team2.logo}`
+                document.querySelector(".game-content .team:last-child p").textContent = game.team2.name
 
-            buyTicketBtnElm.removeAttribute("disabled")
+                buyTicketBtnElm.removeAttribute("disabled")
+            }
         }
-    }
-
-    document.querySelector(".game-wrapper").classList.remove("game-wrapper_empty")
+        document.querySelector(".game-wrapper").classList.remove("game-wrapper_empty")
+    })
 
 
     const ticketCardElm = document.querySelector(".ticket-card")
@@ -94,10 +94,36 @@ async function init() {
             buyTicketBtnElm.removeAttribute("disabled")
         })
     })
+    
+
+    document.querySelectorAll(".control-ticket-number-btn").forEach((button, i) => {
+        const quantityElm = document.querySelector(".quantity-element")
+        button.addEventListener("click", ()=> {
+            let newQuantity = ticket.quantity
+            if (i === 0) newQuantity -= 1
+            else newQuantity += 1
+            if (newQuantity < 1 || newQuantity > 5) return
+            ticket.quantity = newQuantity
+            quantityElm.textContent = "0" + newQuantity
+        })
+    })
 
 
     buyTicketBtnElm.addEventListener("click", async (e) => {
         e.preventDefault()
+
+        if (ticket.gameId == null) {
+            const errNot = new APP.Notification("Please select another match ", "false")
+            errNot.push()
+            errNot.popAfter(2000)
+            return
+        } else if (ticket.bleacherType == null) {
+            const errNot = new APP.Notification("Please select a bleacher", "false")
+            errNot.push()
+            errNot.popAfter(2000)
+            return
+        }
+
         buyTicketBtnElm.setAttribute("disabled", "true")
 
         const waitNot = new APP.Notification("Please wait... ", "loading")
